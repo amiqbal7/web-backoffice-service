a8b25f0b0cf156c21a934f61f97b70a7864c4a61
amiqbal <iqbalmuhammad7578@gmail.com>
2023-09-08 10:00:26 +0700

    upadate users

--- src/pages/dashboard/users/index.tsx
+++ src/pages/dashboard/users/index.tsx
@@ -3,7 +3,6 @@ import type { ColumnsType } from 'antd/es/table';
 import { useApi, useAuth } from '@/hooks';
 import { DataTable } from '@/components';
 import type { User } from '@/types';
-import moment from 'moment';
 import qs from 'qs';
 
 type DataType = User & {
@@ -23,20 +22,23 @@ export function Component() {
       total: number;
       per_page: number;
       current_page: number;
+    };
+  }>(
+    {
+      url: '/users',
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params,
     },
-  }>({
-    url: '/users',
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    params,
-  }, { manual: false });
+    { manual: false }
+  );
   const columns: ColumnsType<DataType> = [
     {
       title: 'No',
       key: 'id',
-      render: (_row, _data, i) => (limit * (page-1))+(i+1),
+      render: (_row, _data, i) => limit * (page - 1) + (i + 1),
       width: 50,
     },
     {
@@ -48,59 +50,40 @@ export function Component() {
       title: 'Nomor telepon',
       dataIndex: 'phone',
       key: 'name',
-      render: (_, { phone }) => phone ? phone : '-',
+      render: (_, { phone }) => (phone ? phone : '-'),
+    },
+
+    {
+      key: 'address',
+      title: 'Address',
+      dataIndex: 'address',
+      render: (_, { address }) => (address ? address : '-'),
     },
     {
-      title: 'Jenis Kelamin',
-      dataIndex: 'gender',
+      key: 'email',
+      title: 'Email',
+      dataIndex: 'email',
+      render: (_, { email }) => (email ? email : '-'),
+    },
+    {
       key: 'gender',
-      render: (_, { gender }) => {
-        switch (gender) {
-          case 'male':
-            return 'Laki - Laki';
-          case 'female':
-            return 'Perempuan';
-          default:
-            return '-';
-        }
-      },
-      filters: [
-        {
-          text: 'Laki - Laki',
-          value: 'male',
-        },
-        {
-          text: 'Perempuan',
-          value: 'female',
-        },
-      ]
+      title: 'Gender',
+      dataIndex: 'gender',
+      render: (_, { gender }) => (gender ? gender : '-'),
     },
     {
-      key: 'height',
-      title: 'Tinggi Badan',
-      dataIndex: 'height',
-      render: (_, { height }) => height ? `${height} cm` : '-',
-    },
-    {
-      key: 'age',
-      title: 'Umur',
-      dataIndex: 'age',
-      render: (_, { age }) => age ? `${age} tahun` : '-',
-    },
-    {
-      key: 'created_at',
-      title: 'Tanggal Register',
-      dataIndex: 'created_at',
-      render: (_, { created_at }) => moment(created_at).format('YYYY-MM-DD HH:mm:ss'),
-      sorter: true,
+      key: 'company',
+      title: 'Company',
+      dataIndex: 'company',
+      render: (_, { company }) => (company ? company : '-'),
     },
   ];
 
   return (
     <DataTable<DataType>
-      title='Users'
-      subtitle='Manage all your existing clients'
-      rowKey='id'
+      title="Users"
+      subtitle="Manage all your existing clients"
+      rowKey="id"
       columns={columns}
       data={data?.data ?? []}
       loading={loading}
